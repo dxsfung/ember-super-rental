e4e05ca5fef697899c1fabb40b10b3257e42e08b
David Fung <davxsfung@gmail.com>
2017-03-07 17:17:56 +0800

    added Demo Shopping Cart using Service

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart: Ember.inject.service(),
 
     sortBy: ['rating:desc'],
     sortedReviews: Ember.computed.sort('rental.reviews', 'sortBy'),
 
   actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    },
     delete(rental) {
       if (confirm('Are you sure you want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
